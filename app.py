a6cb6db37191835a6a2136eb51c74adaebd1deb6
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-09-27 16:25:15 -0700

    Included statements to show flash messages to user.

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from online_store import app
 ########################################################################
 
 # Cart route, checkout
+# Logout button to user template
+# Clean up js files that are no longer being used
+# Refactor admin image upload to use Pillow
 # Contact route to actually submit to something
 # Delete profile functionality
 # Look into "remember me" and "url_is_safe" functionality for UX/security
