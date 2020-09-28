a7712bd70060d220de1041f674b8d89bf67a9262
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-09-28 15:19:53 -0700

    Updated TODOs

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from online_store import app
 ########################################################################
 
 # Cart route, checkout
-# Logout button to user template
 # Contact route to actually submit to something
 # Delete profile functionality
 # Look into "remember me" and "url_is_safe" functionality for UX/security
 # Potentially improve redirect on 404 page to be server-side or JS
-# Design custom icon to display when logged in, nav link title for when not
+# Design custom icon to display when logged in, nav link title for when not ?
 # Button in user route not working now for some reason, figure out why
+# Clean up styling
 
 ########################################################################
 #                   #Run                                               #
