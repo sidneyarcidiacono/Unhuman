fbf491511a3cfd47a2957a73bd05de918e164498
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-04 17:44:25 -0700

    Styling and content updates

--- online_store/static/artwork.js
+++ online_store/static/artwork.js
@@ -1,27 +1,30 @@
 const productImages = document.querySelectorAll('#product-image')
 const seeMoreButtons = document.querySelectorAll('.see-more-icon')
 const cancelAddBtn = document.getElementById('cancel-add-icon')
-const backdrop = document.getElementById('backdrop')
+// const backdrop = document.getElementById('backdrop')
+const gallery = document.querySelector('.gallery')
 
-const toggleBackdropHandler = () => {
-  backdrop.classList.toggle('visible')
-}
+// const toggleBackdropHandler = () => {
+//   backdrop.classList.toggle('visible')
+// }
 
 const showProductModalHandler = productId => {
   const productModal = document.getElementById(productId)
-  toggleBackdropHandler()
+  // toggleBackdropHandler()
+  gallery.classList.toggle('invisible')
   productModal.classList.add('visible')
 }
 
 const closeProductModal = productId => {
   const productModal = document.getElementById(productId)
+  gallery.classList.toggle('invisible')
   productModal.classList.remove('visible')
-  toggleBackdropHandler()
+  // toggleBackdropHandler()
 }
 
 const backdropClickHandler = () => {
   closeProductModal()
-  toggleBackdropHandler()
+  // toggleBackdropHandler()
 }
 
 // window.addEventListener('click', () => {
