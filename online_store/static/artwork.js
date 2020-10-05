1a82c692213cfce6cf2c2fc11fcf774b651323ec
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-04 18:53:09 -0700

    Enabled filtering on artwork page.

--- online_store/static/artwork.js
+++ online_store/static/artwork.js
@@ -3,14 +3,40 @@ const seeMoreButtons = document.querySelectorAll('.see-more-icon')
 const cancelAddBtn = document.getElementById('cancel-add-icon')
 // const backdrop = document.getElementById('backdrop')
 const gallery = document.querySelector('.gallery')
+const galleryItems = document.querySelectorAll('.gallery-item')
+const filter = document.getElementById('media')
+
+console.log(`Filter: ${filter}`)
 
 // const toggleBackdropHandler = () => {
 //   backdrop.classList.toggle('visible')
 // }
 
+const filterGallery = () => {
+  for (galleryItem of galleryItems) {
+    const itemMedia = galleryItem.dataset.media
+    if (filter.value == 'Paintings') {
+      if (itemMedia != 'Oil on Canvas') {
+        galleryItem.classList.add('invisible')
+      } else {
+        galleryItem.classList.remove('invisible')
+      }
+    } else if (filter.value == 'Prints') {
+      if (itemMedia != 'Print') {
+        galleryItem.classList.add('invisible')
+      } else {
+        galleryItem.classList.remove('invisible')
+      }
+    } else if (filter.value == 'Full Gallery' && galleryItem.classList.contains('invisible')) {
+      galleryItem.classList.remove('invisible')
+    }
+  }
+}
+
 const showProductModalHandler = productId => {
   const productModal = document.getElementById(productId)
   // toggleBackdropHandler()
+  filter.classList.toggle('invisible')
   gallery.classList.toggle('invisible')
   productModal.classList.add('visible')
 }
@@ -18,6 +44,7 @@ const showProductModalHandler = productId => {
 const closeProductModal = productId => {
   const productModal = document.getElementById(productId)
   gallery.classList.toggle('invisible')
+  filter.classList.toggle('invisible')
   productModal.classList.remove('visible')
   // toggleBackdropHandler()
 }
@@ -32,3 +59,5 @@ const backdropClickHandler = () => {
 //     backdropClickHandler
 //   }
 // })
+
+filter.addEventListener('change', filterGallery)
