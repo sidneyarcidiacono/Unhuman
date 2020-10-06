67e8e95fc3c6f9ccbd78be626b3920b1273e9def
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-05 21:03:51 -0700

    reconfiguring for deploy.

--- online_store/__init__.py
+++ online_store/__init__.py
@@ -5,27 +5,31 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-import boto3
 
-s3 = boto3.resource("s3")
+# import boto3
 
-from boto3.s3.connection import S3Connection
+# s3 = boto3.resource("s3")
 
-s3 = S3Connection(
-    os.environ["SECRET_KEY"],
-    os.environ["SQLALCHEMY_DATABASE_URI"],
-    os.environ["MAIL_DEFAULT_SENDER"],
-    os.environ["MAIL_PASSWORD"],
-    os.environ["MAIL_USERNAME"],
-    os.environ["STRIPE_SECRET"],
-)
+# from boto3.s3.connection import S3Connection
+
+# s3 = S3Connection(
 
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 mail = Mail(app)
 
-stripe.api_key = S3Connection(os.environ["STRIPE_PUBLIC_KEY"])
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    os.environ["SQLALCHEMY_DATABASE_URI"],
+)
+app.config["MAIL_DEFAULT_SENDER"] = os.environ["MAIL_DEFAULT_SENDER"]
+app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
+app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
+app.config["STRIPE_SECRET"] = os.environ["STRIPE_SECRET"]
+# )
+
+stripe.api_key = os.environ["STRIPE_PUBLIC_KEY"]
 
 # Define flask-login config variables & instantiate LoginManager
 login_manager = LoginManager(app)
